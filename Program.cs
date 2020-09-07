e425f58fb41e768a8477c2404e1179cc96b0044f
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2020-09-07 14:24:55 +0530

    added constants and datasetup

--- Program.cs
+++ Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PromotionEngine
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static Dictionary<string, int> UnitPrices = new Dictionary<string, int>()
         {
+            { "A",50 },
+            { "B",30 },
+            { "C",20 },
+            { "D",15 },
+        };
+
+        private static List<(List<(string SKU, int n)> SKUs, int fixedPrice)> ActivePromotions =
+            new List<(List<(string SKU, int n)> SKUs, int fixedPrice)>();
+
+        private static void Main()
+        {
+            var promotion = (new List<(string SKU, int n)>() { ("A", 3) }, 130);
+            SetPromotion(promotion);
+            promotion = (new List<(string SKU, int n)>() { ("B", 2) }, 45);
+            SetPromotion(promotion);
+            promotion = (new List<(string SKU, int n)>() { ("C", 1), ("D", 1) }, 30);
+            SetPromotion(promotion);
+
+            var order = new Dictionary<string, int> { { "A", 1 }, { "B", 1 }, { "C", 1 } };
+            Console.WriteLine(ApplyPromotion(order));
+            order = new Dictionary<string, int> { { "A", 5 }, { "B", 5 }, { "C", 1 } };
+            Console.WriteLine(ApplyPromotion(order));
+            order = new Dictionary<string, int> { { "A", 3 }, { "B", 5 }, { "C", 1 }, { "D", 1 } };
+            Console.WriteLine(ApplyPromotion(order));
+            Console.ReadKey();
         }
+
+        private static int ApplyPromotion(Dictionary<string, int> order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void SetPromotion((List<(string SKU, int n)> SKUs, int fixedPrice) promotion) => ActivePromotions.Add((promotion.SKUs, promotion.fixedPrice));
     }
 }
